d5bdc64564e7daa144e73efac115ea7f48ef1ce2
williamgyamfik <chester3gh@gmail.com>
2023-08-17 21:07:30 +0300

    modified detail page

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import { context } from "@/store/context";
 import { useContext } from "react";
 import CardDetails from "@/components/CardDetails";
-import SearchBar from "@/components/SearchBar";
 
 const Index = (props) => {
   const router = useRouter();
@@ -15,7 +14,6 @@ const Index = (props) => {
   return (
     <>
       <div className=" space-y-7 flex flex-col flex-grow">
-        {/* <SearchBar /> */}
         <CardDetails postDetail={postDetail} />
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,4 @@
-import Navbar from "@/components/NavBar";
-import { useContext } from "react";
-import React from "react";
-import PostList from "@/components/PostList";
-import { context } from "@/store/context";
-
-const Index = ({ posts }) => {
-  // const { post, setPost } = useContext(context);
-
-  // if (posts) {
-  //   setPost(posts);
-  // }
+const Index = () => {
   return <></>;
 };
 
